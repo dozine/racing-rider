398ac9d4f673dc0ed73a59aee798b5308dc0b5ec
dozin <dojin0345@gmail.com>
2025-11-18 15:46:03 +0900

    refactor : 라이더 모델링 다루기 위한 라이더 매니저 분리

---
+++ src/RiderManager.js
@@ -0,0 +1,65 @@
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+export default class RiderManager {
+  constructor(scene, numRiders, initialZPosition, modelPath) {
+    this.scene = scene;
+    this.numRiders = numRiders;
+    this.initialZPosition = initialZPosition;
+    this.modelPath = modelPath;
+    this.riderMeshes = [];
+    this.loader = new GLTFLoader();
+    this.onAllModelsLoaded = null;
+  }
+
+  loadAllRiders() {
+    return new Promise((resolve) => {
+      let loadedCount = 0;
+
+      for (let i = 0; i < this.numRiders; i++) {
+        this.loadRiderModel(i, () => {
+          loadedCount++;
+          if (loadedCount === this.numRiders) {
+            resolve();
+          }
+        });
+      }
+    });
+  }
+
+  loadRiderModel(index, onLoad) {
+    this.loader.load(
+      this.modelPath,
+      (gltf) => {
+        const riderMesh = gltf.scene;
+        riderMesh.scale.set(0.5, 0.5, 0.5);
+        riderMesh.position.set(
+          (index - (this.numRiders - 1) / 2) * 2,
+          0.2,
+          this.initialZPosition
+        );
+
+        this.scene.add(riderMesh);
+        this.riderMeshes[index] = riderMesh;
+
+        if (onLoad) onLoad();
+      },
+      undefined,
+      (error) => {
+        console.error("모델 로딩 중 오류 발생:", error);
+      }
+    );
+  }
+
+  updateRiderPositions(cars) {
+    cars.forEach((car, index) => {
+      const mesh = this.riderMeshes[index];
+      if (mesh) {
+        mesh.position.z = this.initialZPosition + car.distance;
+      }
+    });
+  }
+
+  getRiderNames() {
+    return Array.from({ length: this.numRiders }, (_, i) => `Rider ${i + 1}`);
+  }
+}

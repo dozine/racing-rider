f90dadb57056d3935a3018fbf6de2f5a23e0e8a4
dozin <dojin0345@gmail.com>
2025-11-18 15:47:21 +0900

    refactor : 전체 게임 관리를 위한 게임 컨트롤러 생성

---
+++ src/GameController.js
@@ -0,0 +1,67 @@
+import RacingGame from "./RacingGame.js";
+
+export default class GameController {
+  constructor(sceneManager, riderManager, uiManager, config) {
+    this.sceneManager = sceneManager;
+    this.riderManager = riderManager;
+    this.uiManager = uiManager;
+    this.config = config;
+
+    this.racingGame = null;
+    this.lastUpdateTime = 0;
+    this.winnerDeclared = false;
+  }
+
+  async init() {
+    await this.riderManager.loadAllRiders();
+
+    const riderNames = this.riderManager.getRiderNames();
+    this.racingGame = new RacingGame(
+      riderNames,
+      this.config.shouldMove,
+      this.config.finishDistance
+    );
+
+    this.uiManager.enableStartButton();
+    this.uiManager.setOnStartCallback(() => this.startGame());
+  }
+
+  startGame() {
+    if (this.racingGame && this.racingGame.gameStatus === "READY") {
+      this.racingGame.startGame();
+      this.uiManager.hideStartButton();
+    }
+  }
+
+  update(currentTime) {
+    if (!this.racingGame) return;
+
+    if (this.racingGame.gameStatus === "RUNNING") {
+      if (currentTime - this.lastUpdateTime > this.config.turnInterval) {
+        this.racingGame.playTurn();
+
+        if (this.racingGame.finish(this.config.finishDistance)) {
+          console.log("경주 종료! 우승자를 확인합니다.");
+        }
+
+        this.lastUpdateTime = currentTime;
+      }
+
+      this.riderManager.updateRiderPositions(this.racingGame.cars);
+    } else if (
+      this.racingGame.gameStatus === "FINISHED" &&
+      !this.winnerDeclared
+    ) {
+      const results = this.racingGame.getRaceResults();
+      this.uiManager.showResults(results);
+      this.winnerDeclared = true;
+    }
+  }
+
+  animate(currentTime) {
+    requestAnimationFrame((time) => this.animate(time));
+
+    this.update(currentTime);
+    this.sceneManager.render();
+  }
+}
